e55dcd704c5f884ebbea083511ccf1b9ff476f9a
Raiyan109 <kraiyan109@gmail.com>
2022-05-22 08:26:37 +0600

    navbar and banner done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          primary: "#5f4d5d",
+
+          secondary: "#d7b7d4",
+
+          accent: "#b0b0b0",
+
+          neutral: "#e6e6e6",
+
+          "base-100": "#FFFFFF",
+
+          info: "#b08ead",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
