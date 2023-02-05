2078db4ad5df119924d76f55d021b1b02bd6e8cb
Raiyan109 <kraiyan109@gmail.com>
2023-02-05 16:05:24 +0600

    emailjs

--- src/Pages/Home/StayConnected.js
+++ src/Pages/Home/StayConnected.js
@@ -1,6 +1,31 @@
-import React from 'react';
-
+import React, { useRef } from 'react';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 const StayConnected = () => {
+    // const [formState, setFormState] = useState({})
+
+    // const changeHandler = e => {
+    //     setFormState({ ...formState, [e.target.name]: e.target.value })
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault()
+    // }
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_t3rlyd6', 'template_k8rrwcu', form.current, 'W_0i0So-npwUb6b5D')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset()
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className='bg-primary px-10 py-14 '>
             <div className='text-center pb-14 text-white'>
@@ -9,24 +34,30 @@ const StayConnected = () => {
                 </p>
                 <h1 className='text-4xl'>Stay connected with us</h1>
             </div>
-            <div className='grid grid-cols-1 justify-items-center gap-5'>
-                <input
-                    type='text'
-                    placeholder='Email Address'
-                    className='input w-full max-w-md'
-                />
-                <input
-                    type='text'
-                    placeholder='Subject'
-                    className='input w-full max-w-md'
-                />
-                <textarea
-                    className='textarea w-full max-w-md'
-                    placeholder='Your message'
-                    rows={6}
-                ></textarea>
+            <form ref={form} onSubmit={sendEmail}>
+                <div className='grid grid-cols-1 justify-items-center gap-5'>
+                    <input
+                        type='text'
+                        name="user_name"
+                        placeholder='Name'
 
-            </div>
+                        className='input w-full max-w-md'
+                    />
+                    <input
+                        type='text'
+                        placeholder='Email Address'
+                        name="user_email"
+                        className='input w-full max-w-md'
+                    />
+                    <textarea
+                        className='textarea w-full max-w-md'
+                        placeholder='Your message'
+                        name="message"
+                        rows={6}
+                    ></textarea>
+                    <button type='submit' value="Send" class="btn btn-secondary">Send</button>
+                </div>
+            </form>
         </div>
     );
 };
