92b2212a3012d96757b0c32f1e992d1f80f51a9a
Raiyan109 <kraiyan109@gmail.com>
2023-03-13 07:33:39 +0600

    quote page reload

--- src/Pages/GetQuote/GetQuote.js
+++ src/Pages/GetQuote/GetQuote.js
@@ -26,30 +26,17 @@ const GetQuote = () => {
     }
     return (
         <div>
-            <section className="text-gray-600 body-font">
-                <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-
-                    </div>
-                    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                        <img className="object-cover object-center rounded" alt="hero" src={img} />
-                    </div>
-                </div>
-            </section>
-
-
-
-            <div class="bg-white relative lg:py-20">
-                <div class="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
+            <div className="bg-white relative lg:py-20">
+                <div className="flex flex-col items-center justify-between pt-0 pr-10 pb-0 pl-10 mt-0 mr-auto mb-0 ml-auto max-w-7xl
       xl:px-5 lg:flex-row">
-                    <div class="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-20 lg:flex-row">
-                        <div class="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
-                            <div class="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
-                                <img src="https://res.cloudinary.com/macxenon/image/upload/v1631570592/Run_-_Health_qcghbu.png" alt='multi-step-background' class="btn-" />
+                    <div className="flex flex-col items-center w-full pt-5 pr-10 pb-20 pl-10 lg:pt-20 lg:flex-row">
+                        <div className="w-full bg-cover relative max-w-md lg:max-w-2xl lg:w-7/12">
+                            <div className="flex flex-col items-center justify-center w-full h-full relative lg:pr-10">
+                                <img src="https://res.cloudinary.com/macxenon/image/upload/v1631570592/Run_-_Health_qcghbu.png" alt='multi-step-background' className="btn-" />
                             </div>
                         </div>
-                        <div class="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">
-                            <div class="flex flex-col items-start justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl
+                        <div className="w-full mt-20 mr-0 mb-0 ml-0 relative z-10 max-w-2xl lg:mt-0 lg:w-5/12">
+                            <div className="flex flex-col items-start justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl
             relative z-10">
                                 <div className='flex justify-center items-center mx-auto mb-10'>
                                     <progress className="progress progress-primary w-56" value={page === 0 ? '20' : page === 1 ? '40' : page === 2 ? '70' : '100'} max="100"></progress>
@@ -60,8 +47,8 @@ const GetQuote = () => {
                             <li className="step">Receive Product</li>
                         </ul> */}
                                 </div>
-                                <p class="w-full text-4xl font-medium text-center leading-snug font-serif">{formTitles[page]}</p>
-                                <div class="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
+                                <p className="w-full text-4xl font-medium text-center leading-snug font-serif">{formTitles[page]}</p>
+                                <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
 
                                     <div className='h-80'>
                                         {/* Head */}
@@ -91,14 +78,9 @@ const GetQuote = () => {
                                             </div>
                                         </div>
                                     </div>
-
-
-                                    {/*  */}
-
-                                    {/*  */}
                                 </div>
                             </div>
-                            <svg viewbox="0 0 91 91" class="absolute top-0 left-0 z-0 w-32 h-32 -mt-12 -ml-12 text-yellow-300
+                            <svg viewbox="0 0 91 91" className="absolute top-0 left-0 z-0 w-32 h-32 -mt-12 -ml-12 text-yellow-300
             fill-current"><g stroke="none" strokewidth="1" fillrule="evenodd"><g fillrule="nonzero"><g><g><circle
                                     cx="3.261" cy="3.445" r="2.72" /><circle cx="15.296" cy="3.445" r="2.719" /><circle cx="27.333" cy="3.445"
                                         r="2.72" /><circle cx="39.369" cy="3.445" r="2.72" /><circle cx="51.405" cy="3.445" r="2.72" /><circle cx="63.441"
@@ -128,7 +110,7 @@ const GetQuote = () => {
                                                         r="2.719" /><circle cx="27.333" cy="3.006" r="2.72" /><circle cx="39.369" cy="3.006" r="2.72" /><circle
                                             cx="51.405" cy="3.006" r="2.72" /><circle cx="63.441" cy="3.006" r="2.72" /><circle cx="75.479" cy="3.006"
                                                 r="2.72" /><circle cx="87.514" cy="3.006" r="2.719" /></g></g></g></g></svg>
-                            <svg viewbox="0 0 91 91" class="absolute bottom-0 right-0 z-0 w-32 h-32 -mb-12 -mr-12 text-indigo-500
+                            <svg viewbox="0 0 91 91" className="absolute bottom-0 right-0 z-0 w-32 h-32 -mb-12 -mr-12 text-indigo-500
             fill-current"><g stroke="none" strokewidth="1" fillrule="evenodd"><g fillrule="nonzero"><g><g><circle
                                     cx="3.261" cy="3.445" r="2.72" /><circle cx="15.296" cy="3.445" r="2.719" /><circle cx="27.333" cy="3.445"
                                         r="2.72" /><circle cx="39.369" cy="3.445" r="2.72" /><circle cx="51.405" cy="3.445" r="2.72" /><circle cx="63.441"
