43c282f5e76921b8064efcc754c394d17baf75d4
Raiyan109 <kraiyan109@gmail.com>
2023-03-21 10:40:44 +0600

    my orders

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,9 +3,9 @@ import { NavLink, Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <div class="drawer drawer-mobile mt-16 bg-accent">
+        <div class="drawer drawer-mobile mt-16">
             <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
+            <div class="drawer-content">
                 {/* <!-- Page content here --> */}
                 <Outlet />
 
--- src/Pages/Home/TestimonialReview.js
+++ src/Pages/Home/TestimonialReview.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import { RxAvatar } from "react-icons/rx";
 
 const TestimonialReview = ({ review }) => {
     return (
         <div className="card lg:max-w-lg bg-base-100 shadow-xl">
             <div className="card-body">
-                <p>{review.review}</p>
+
                 <div className="flex items-center">
                     <div className="avatar">
                         <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
-                            <img src={review.img} />
+                            {review.img ?
+                                <img src={review.img} alt={review.name}
+                                />
+                                :
+                                <RxAvatar className='w-16 h-16' />
+                            }
                         </div>
                     </div>
                     <div>
                         <h1 className='text-xl'>{review.name}</h1>
+                        <p className='text-sm'>{review.review}</p>
                         <p>{review.location}</p>
                     </div>
                 </div>
             </div>
         </div>
+
+
     );
 };
 
