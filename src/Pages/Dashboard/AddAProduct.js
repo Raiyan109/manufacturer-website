285d4edcf94c05991bfdb1e646223a5b9d613978
Raiyan109 <kraiyan109@gmail.com>
2024-02-06 08:32:57 +0600

    now without hook form

--- src/Pages/Dashboard/AddAProduct.js
+++ src/Pages/Dashboard/AddAProduct.js
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import fetcher from '../../api';
 import axios from "axios";
 
 const AddAProduct = () => {
-
+    const [file, setFile] = useState([])
+    const [showFiles, setShowFiles] = useState([])
     const [imageUrl, setImageUrl] = useState('')
     const [loading, setLoading] = useState(false)
+    // const [name, setName] = useState('')
+    // const [desc, setDesc] = useState('')
+    // const [availableQuantity, setAvailableQuantity] = useState('')
+    // const [minimumOrderQuantity, setMinimumOrderQuantity] = useState('')
+    // const [price, setPrice] = useState('')
 
     const { register, handleSubmit, reset } = useForm();
 
+    // const handleChange = (e) => {
+    //     setFile(e.target.files)
+    //     console.log(e.target.files);
+    //     const filesList = e.target.files
+    //     const fileNames = []
+    //     for (let i = 0; i < filesList.length; i++) {
+    //         fileNames.push(filesList[i].name)
+    //     }
+    //     setShowFiles([...showFiles, ...fileNames])
+    // }
+
     const onSubmit = async (data) => {
-        console.log('clicked from add prodcu');
-        const productData = {
-            ...data,
-            image: imageUrl,
+        const formData = new FormData();
+        // Append other form data
+        formData.append('name', data.name);
+        formData.append('price', data.price);
+        formData.append('desc', data.desc);
+        formData.append('availableQuantity', data.availableQuantity);
+        formData.append('minimumOrderQuantity', data.minimumOrderQuantity);
 
+        // Append file data
+        formData.append('file', data.file[0]); // Assuming single file upload
+        console.log(data);
+        console.log(formData);
+        try {
+            // const res = await fetcher.post('api/parts', formData, {
+            //     headers: {
+            //         'Content-Type': 'multipart/form-data'
+            //     }
+            // })
+            // console.log(res);
+            reset()
+            setImageUrl('')
+        } catch (error) {
+            console.log(error);
         }
-        const res = await fetcher.post('api/parts', productData)
-        console.log(res);
-        reset()
-        setImageUrl('')
     }
 
-    const handleUploadImage = (event) => {
-        setLoading(true)
-        const image = event.target.files[0];
-        const formData = new FormData()
-        formData.set('image', image)
-        axios.post('https://api.imgbb.com/1/upload?key=4cf62e42525cbf72d4c7ec85ca313c4a', formData)
-            .then((res) => {
-                setImageUrl(res.data.data.display_url)
-                setLoading(false)
-            }).catch((error) => {
-                console.log(error);
-            })
-    }
+
     return (
         <div>
             <div className='my-2  flex justify-center items-center'>
@@ -44,29 +63,43 @@ const AddAProduct = () => {
                         Add Product
                     </div>
                     <div className='mt-16 flex flex-col justify-center'>
-                        <div className='flex flex-col lg:flex-row items-center gap-2'>
-                            <input placeholder='Name' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3 ' {...register("name", { required: true, maxLength: 20 })} />
+                        <div className='flex flex-col lg:flex-row items-center gap-2 mb-0 lg:mb-2'>
+                            <input placeholder='Name' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 w-full'
+                                {...register("name", { required: true, maxLength: 20 })}
+                            />
 
-                            <input placeholder='Price' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3 lg:max-w-[215px]' type="number" {...register("price")} />
-                        </div>
-
-                        <textarea placeholder='Description' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3' {...register("description")} />
-
-                        <div className='flex flex-col lg:flex-row items-center gap-2'>
-                            <input placeholder='Available Quantity' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3' {...register("availableQuantity")} />
-
-                            <input placeholder='Min. Order Quantity' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3' type="number" {...register("minOrderQuantity")} />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="image">Upload Image </label>
-                            <input placeholder='Photo URL' id='image' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3' type="file"
-                                onChange={handleUploadImage}
+                            <input placeholder='Price' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-2 lg:mb-0 lg:max-w-[215px] w-full' type="number"
+                                {...register("price")}
                             />
                         </div>
 
+                        <textarea placeholder='Description' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-2'
+                            {...register("desc")}
+                        />
+
+                        <div className='flex flex-col lg:flex-row items-center gap-2 mb-2'>
+                            <input placeholder='Available Quantity' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 w-full'
+                                {...register("availableQuantity")}
+                            />
+
+                            <input placeholder='Min. Order Quantity' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-2 lg:mb-0 w-full' type="number"
+                                {...register("minimumOrderQuantity")}
+                            />
+                        </div>
+                        <div className="form-control">
+                            <label className=''>Upload Image </label>
+                            <input placeholder='Photo' className='text-gray-700 shadow border rounded border-gray-300 focus:outline-none focus:shadow-outline py-1 px-3 mb-3'
+                                type="file"
+                                // name='file'
+                                {...register('file')}
+                            // onChange={handleChange}
+                            />
+                        </div>
+                        <input type="file" {...register('file')} />
                         <input className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4'
                             // disabled={!imageUrl ? true : false}
-                            type="submit" value='Add new item' />
+                            type="submit" value='Add' />
+
                     </div>
                 </form>
             </div>
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
-
+let URL;
+switch (process.env.REACT_APP_ENVIRONMENT) {
+    case "DEVELOPMENT":
+        URL = 'http://localhost:5000/';
+        break;
+    case "PRODUCTION":
+        URL = 'https://leviathan-server.vercel.app/';
+        break;
+    default:
+        URL = 'http://localhost:5000/';
+}
 const fetcher = axios.create({
-    // baseURL: 'https://assignment12-server-chi.vercel.app/',
-    baseURL: 'https://leviathan-server.vercel.app/',
-    // baseURL: 'http://localhost:5000/',
+    baseURL: URL
 });
 
 export default fetcher
