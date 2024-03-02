5a222e6928bbb17258f5869bc6ff8950e92cc022
Raiyan109 <kraiyan109@gmail.com>
2024-03-02 11:19:32 +0600

    photo file

--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -12,7 +12,7 @@ const Part = ({ part }) => {
     return (
         <>
             <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
-                <div className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{ backgroundImage: `url(${`http://localhost:5000/${photo}`})` }}></div>
+                <div className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{ backgroundImage: `url(${`https://leviathan-server-1.onrender.com/${photo}`})` }}></div>
 
                 <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
                     <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">{name}</h3>
